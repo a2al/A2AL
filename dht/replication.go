d62dc879c65342b60ae280c3a74e677e9511e83d
shi <shi@ucomobile.com>
2026-04-19 20:04:11 +0800

    change good node's PING duration maximum value to longer time, reduce PING times/minute from 30+ to <5

--- dht/replication.go
+++ dht/replication.go
@@ -19,7 +19,7 @@ const (
 	nRep              = 8                // soft replication target N_rep
 	repHardCap        = routing.K        // hard cap = K = 16
 	probeInitDelay    = 30 * time.Second // Unknown node: initial probe interval
-	probeMaxDelay     = 10 * time.Minute // Good node: ceiling for exponential back-off
+	probeMaxDelay     = 1 * time.Hour    // Good node: ceiling; in practice StoreAt (every 30 min) contacts the node first
 	probeBadDelay     = 30 * time.Minute // Bad node: grace window before eviction
 	probeTickInterval = 15 * time.Second // health probe loop wake-up interval
 	replChBuf         = 64              // replication task channel buffer
